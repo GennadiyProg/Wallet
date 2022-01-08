05fd73f00bf90efe2cfba898c9a6a45e55400de0
GennadiyProg <grumancev9@gmail.com>
2022-01-08 15:43:40 +0300

    Adding README and file processing

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.ConcurrentHashMap;
@@ -9,9 +10,48 @@ import java.util.regex.Pattern;
 public class Wallet {
     private static ConcurrentMap<String, Long> wallet = new ConcurrentHashMap<>();
 
+    public static void addPayment(String payment){
+        Matcher matcher = Pattern.compile("(?<currency>\\w+)(?:\\s+(?<money>-?\\w+))?").matcher(payment);
+        if (matcher.find()){
+            if (matcher.group("currency").equals("quit")) System.exit(1);
+            if (matcher.group("currency").length() != 3){
+                System.out.printf("Incorrect currency: %s\n", matcher.group("currency"));
+                return;
+            }
+            try{
+                long money = Long.parseLong(matcher.group("money"));
+                String currency = matcher.group("currency").toUpperCase();
+                if (wallet.containsKey(currency)) {
+                    if (wallet.get(currency) + money < 0) {
+                        System.out.println("Not enough money!");
+                    } else {
+                        wallet.computeIfPresent(currency, (k, v) -> v + money);
+                    }
+                } else {
+                    if (money < 0){
+                        System.out.println("Not enough money!");
+                    } else {
+                        wallet.put(currency, money);
+                    }
+                }
+            } catch (NumberFormatException e){
+                System.out.printf("Wrong number: %s\n", matcher.group("money"));
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+        if (args.length > 0) {
+            String inputFileName = args[0];
+            BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                addPayment(line);
+            }
+        }
         Runnable printBalance = () -> {
             while(true) {
+                System.out.println();
                 wallet.forEach((k, v) -> {
                     if (v != 0) System.out.println(k + " " + v);
                 });
@@ -27,29 +67,7 @@ public class Wallet {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while(true){
             String console = reader.readLine();
-            Matcher matcher = Pattern.compile("(?<currency>\\w+)(?:\\s+(?<money>-?\\d+))?").matcher(console);
-            if (matcher.find()){
-                if (matcher.group("currency").equals("quit")) System.exit(1);
-                if (matcher.group("currency").length() != 3){
-                    System.out.printf("Incorrect currency %s\n", matcher.group("currency"));
-                    continue;
-                }
-                try{
-                    long money = Long.parseLong(matcher.group("money"));
-                    String currency = matcher.group("currency").toUpperCase();
-                    if (wallet.containsKey(currency)) {
-                        if (wallet.get(currency) + money < 0) {
-                            System.out.println("Not enough money!");
-                        } else {
-                            wallet.computeIfPresent(currency, (k, v) -> v + money);
-                        }
-                    } else {
-                        wallet.put(currency, money);
-                    }
-                } catch (NumberFormatException e){
-                    System.out.printf("Wrong number: %s\n", matcher.group("money"));
-                }
-            }
+            addPayment(console);
         }
     }
 }
